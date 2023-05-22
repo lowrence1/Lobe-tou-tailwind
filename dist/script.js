ecf8ba3ac9cceecf0f7a399b697cb323e7dbcfd6
kcryns <rayneskarlcydrick@gmail.com>
2023-05-22 15:34:24 +0800

    2nd commit

--- dist/script.js
+++ dist/script.js
@@ -4,3 +4,13 @@ toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerBot.classList.toggle("open");
 });
+
+
+// ====DROP DOWN LINKS===//
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
