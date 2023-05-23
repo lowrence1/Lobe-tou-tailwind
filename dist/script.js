d79f6d56bb481d5211ca5210998cabfef03734ce
kcryns <rayneskarlcydrick@gmail.com>
2023-05-23 15:06:11 +0800

    latest commit

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,12 @@
+// ====DROP DOWN LINKS===//
+const faqs = document.querySelectorAll(".faq__question h4");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.nextElementSibling.classList.toggle("open");
+    faq.querySelector("i").classList.toggle("open");
+  });
+});
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerBot = document.querySelector(".header__actions");
 toggleMenu.addEventListener("click", () => {
@@ -7,10 +16,12 @@ toggleMenu.addEventListener("click", () => {
 
 
 // ====DROP DOWN LINKS===//
-const links = document.querySelectorAll(".links__drop__item h4");
+const links = document.querySelectorAll(".footer__drop__item h4");
 links.forEach((baba) => {
   baba.addEventListener("click", () => {
     baba.nextElementSibling.classList.toggle("open");
     baba.querySelector("i").classList.toggle("open");
   });
 });
+
+
